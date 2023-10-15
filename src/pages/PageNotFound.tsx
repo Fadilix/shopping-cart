14e5c1f67e70d31fc9395f5a3fbdda1530fbafbb
Fadilix <iamfadile2@gmail.com>
2023-10-15 18:08:52 +0000

    added page not found

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,18 +1,22 @@
 import { motion } from "framer-motion"
 import "../scss/PageNotFound.scss"
+import NavBar from "../components/NavBar"
 
 const PageNotFound: React.FC = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0, y: -100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{duration: 2}}
-            className="pnf"
-        >
-            <p>404</p>
-            <div className="vertical-line"></div>
-            <p>PAGE NOT FOUND</p>
-        </motion.div>
+        <>
+            <NavBar />
+            <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2 }}
+                className="pnf"
+            >
+                <p>404</p>
+                <div className="vertical-line"></div>
+                <p>PAGE NOT FOUND</p>
+            </motion.div>
+        </>
     )
 }
 
